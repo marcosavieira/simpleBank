767211a817fb5d8053f4763649c61ea41fbe77a2
devmarcosav <devmarcosav@gmail.com>
2023-11-27 18:12:27 -0300

    feat: create jwt, paseto token and test

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,63 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/marcosavieira/simpleBank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestExpiredPasetToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidPasetoTokenAlgNone(t *testing.T) {
+	maker, _ := NewPasetoMaker(util.RandomString(32))
+	token := "v2.local."
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrInvalidToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidKeySize(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(30))
+	require.Error(t, err)
+	require.EqualError(t, err, "invalid key size: must be exactly 32 characters")
+	require.Nil(t, maker)
+}
