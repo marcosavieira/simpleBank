27176b11e37e6a56f5c8f8f272c2c30c11d06fca
devmarcosav <devmarcosav@gmail.com>
2023-11-27 12:39:48 -0300

    feat: create a hash password

---
+++ util/password.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	passwordHashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %w", err)
+
+	}
+	return string(passwordHashed), nil
+}
+
+func CheckPassword(password string, hashedpassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedpassword), []byte(password))
+
+}
